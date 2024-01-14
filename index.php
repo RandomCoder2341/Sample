0d39b4aeea86637ec2b007165bb794055821dbce
RandomCoder2341 <151423639+RandomCoder2341@users.noreply.github.com>
2024-01-14 19:13:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initaial-scale=1.0">
+
+<body>
+ <h1> this is the body </h1>
+
+ <?php
+ echo"hello world from php";
+
+?>
+
+
+</body>
+</html>
